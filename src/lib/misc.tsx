d44469d0e66be781d25bbadbcfbed2819fcd04d9
Johannes Dumoli Tambunan <dumoli13@gmail.com>
2024-04-25 14:45:34 +0700

    Create Jest Unit Test for misc function

--- src/lib/misc.tsx
+++ src/lib/misc.tsx
@@ -3,14 +3,19 @@ export function createQueryString({
   queryObject,
 }: {
   searchParams?: string | string[][] | Record<string, string> | URLSearchParams;
-  queryObject: { [key: string]: string };
+  queryObject: { [key: string]: string | string[] };
 }): string {
   const params = new URLSearchParams(searchParams);
   Object.keys(queryObject).forEach((key) => {
     if (queryObject[key] === '') {
       params.delete(key);
     } else {
-      params.set(key, queryObject[key]);
+      const value = queryObject[key];
+      if (Array.isArray(value)) {
+        value.forEach((v) => params.append(key, v));
+      } else {
+        params.set(key, value);
+      }
     }
   });
 
